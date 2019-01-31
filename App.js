05d9d257782c0fe2d6aef2db97e981247cbdc4ac
joe <kinjoyuji@gmail.com>
2019-02-01 00:59:47 +0900

    navigation実装

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './screens/Home';
+import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Home/>
-      </View>
+      <AppNavigator/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/Home';
+import ChatScreen from '../screens/Chat';
+import MatchesScreen from '../screens/Matches';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const ChatStack = createStackNavigator({
+  Chat: ChatScreen,
+});
+
+ChatStack.navigationOptions = {
+  tabBarLabel: 'Chat',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const MatchesStack = createStackNavigator({
+  Matches: MatchesScreen,
+});
+
+MatchesStack.navigationOptions = {
+  tabBarLabel: 'Matches',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  ChatStack,
+  MatchesStack,
+});
