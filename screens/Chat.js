e3919de72eb62336106e9f36d01366f47f59d2d6
joe <kinjoyuji@gmail.com>
2019-02-01 16:07:53 +0900

    react-native-gifted-chatの導入

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import styles from '../styles';
+// 参考
+// https://qiita.com/Yorinton/items/b029df0713a471d4569a
+import React, {Component} from 'react';
+import { GiftedChat　} from 'react-native-gifted-chat';
 
-import { 
-  Text, 
-  View
-} from 'react-native';
+export default class Chat extends Component {
 
-class Chat extends React.Component {
-  state = {}
-
-  componentWillMount() {}
+  //メッセージ内容をstateで管理
+  state = {
+    messages:[],
+  };
+  //「Send」ボタンが押された時に実行されるメソッド
+  onSend = (messages = []) => {
+    this.setState((previousState) => ({
+      //stateで管理しているmessagesに送信されたメッセージを追加
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
 
   render() {
     return (
-     <View>
-      <Text>Chat</Text>
-     </View>
+      //react-native-gifted-chatが提供するコンポーネント
+      <GiftedChat
+        messages={this.state.messages}//stateで管理しているメッセージ
+        onSend={(messages) => this.onSend(messages)}//送信ボタン押した時の動作
+        user={{
+        _id: 1,
+        }}
+      />
     )
   }
 }
-
-export default Chat;
