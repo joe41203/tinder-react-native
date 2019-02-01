5a170e5b42224ec1c95e8a45b61b102db8af0d8b
joe <kinjoyuji@gmail.com>
2019-02-01 17:01:47 +0900

    login画面実装

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
+import LoginScreen from './screens/Login';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return (
-      <AppNavigator/>
-    );
+    const isLogin = false;
+
+    if (isLogin) {
+      return (<AppNavigator/>);
+    } else {
+      return (<LoginScreen/>);
+    }
   }
 }
