ce81dbee54179b45e2ff1fee10bb400abfb593cc
joe <kinjoyuji@gmail.com>
2019-02-01 18:54:02 +0900

    firebaseとの連携

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
 import LoginScreen from './screens/Login';
+import firebase from 'firebase';
+import firenaseConfig from './config/firebase';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    firebase.initializeApp(firenaseConfig);
   }
 
   render() {
-    const isLogin = false;
+    
+    const currentUser = firebase.auth().currentUser;
+    console.log('currentUser', currentUser);
 
-    if (isLogin) {
+    if (currentUser) {
       return (<AppNavigator/>);
     } else {
       return (<LoginScreen/>);
